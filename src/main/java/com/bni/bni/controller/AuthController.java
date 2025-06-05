c9e5a89f1ec58c1cbde275574e7cb6601e42f227
sannugraha20 <alikhsannugraha20@gmail.com>
2025-06-05 14:34:34 +0700

    Login Success Again

--- src/main/java/com/bni/bni/controller/AuthController.java
+++ src/main/java/com/bni/bni/controller/AuthController.java
@@ -43,7 +43,7 @@ public class AuthController {
         if (token != null) {
             response.put("status", 200);
             response.put("token", token);
-            response.put("message", "Login successful");
+            response.put("message", "Login successful Again");
             return ResponseEntity.ok(response);
         } else {
             response.put("status", 401);
