177870580dd6d30cf7d0c38d175ae3545b5b87b6
sannugraha20 <alikhsannugraha20@gmail.com>
2025-06-10 11:58:19 +0700

    UploadShow

--- src/main/java/com/bni/bni/controller/FileController.java
+++ src/main/java/com/bni/bni/controller/FileController.java
@@ -2,7 +2,11 @@ package com.bni.bni.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -55,35 +59,29 @@ public class FileController {
         }
     }
 
-    @GetMapping("/{fileName:.+}")
-    public ResponseEntity<?> getFile(@PathVariable String fileName) {
+    @GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
         try {
-            Path filePath = Paths.get(uploadDir).resolve(fileName).normalize();
-            if (!Files.exists(filePath)) {
-                return ResponseEntity.status(404).body(
-                        Map.of(
-                                "status", 404,
-                                "message", "File not found"
-                        )
-                );
+            Path filePath = Paths.get(uploadDir).resolve(filename).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (!resource.exists()) {
+                return ResponseEntity.notFound().build();
             }
 
-            byte[] fileContent = Files.readAllBytes(filePath);
-            return ResponseEntity.ok().body(
-                    Map.of(
-                            "status", 200,
-                            "fileName", fileName,
-                            "fileContent", fileContent
-                    )
-            );
-        } catch (IOException e) {
-            return ResponseEntity.status(500).body(
-                    Map.of(
-                            "status", 500,
-                            "message", "Failed to retrieve file: " + e.getMessage()
-                    )
-            );
-        }
+            String contentType = Files.probeContentType(filePath);
+            if (contentType == null) {
+                contentType = "application/octet-stream";
+            }
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+    }
     }
     
 
